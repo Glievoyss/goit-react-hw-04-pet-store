241d36495a0db65d8d22ab5e98b0c888c3c9d5ae
Glievoyss <glievoy.s@gmail.com>
2019-10-22 04:50:03 +0300

    Updates

---
+++ precache-manifest.b0765a0b3bea4ad6f44a8ffbb0149ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08221990012c53b029537afc94b7b59",
+    "url": "/goit-react-hw-04-pet-store/./index.html"
+  },
+  {
+    "revision": "6d9761f8f2411d4884bb",
+    "url": "/goit-react-hw-04-pet-store/./static/css/main.915587bb.chunk.css"
+  },
+  {
+    "revision": "854e087f63af9b9b034a",
+    "url": "/goit-react-hw-04-pet-store/./static/js/2.2e7f6861.chunk.js"
+  },
+  {
+    "revision": "6d9761f8f2411d4884bb",
+    "url": "/goit-react-hw-04-pet-store/./static/js/main.54e3f6cd.chunk.js"
+  },
+  {
+    "revision": "1b4daa28a70761f19cde",
+    "url": "/goit-react-hw-04-pet-store/./static/js/runtime-main.ba937869.js"
+  }
+]);
